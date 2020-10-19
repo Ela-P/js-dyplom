f5105e2fd759242b86d6d87b8bad370df72ddd86
Elena P <elaina.prokudova@gmail.com>
2020-10-19 17:37:11 +0300

    Add total users, template

--- src/clients/clientsTotal.js
+++ src/clients/clientsTotal.js
@@ -1,34 +1,47 @@
-export function makeClientsTotal(arrUsers, arrUsersView) {
+const template = `<div class = "makeUsersTotal"> 
+    <span id= "userTotal">
+        <p id="male"></p>
+        <p id="female"></p>
+        <p id="UsersBalance"></p>
+    </span>
+    </div>
+    `;
+
+
+    function makeUsersTotal(arrUsers, arrUsersView)    {
+    
     let genderMaleTotal = 0;
     let genderFemaleTotal = 0;
     let maxTotalUsersBalance = 0;
 
-    const span = document.createElement('span');
+
     for (let i = 0; i < arrUsers.length; i++) {
         if (arrUsers[i].gender === "male") {
             genderMaleTotal = genderMaleTotal + 1;
-            const pMale =  document.createElement("p");
-            pMale.innerHTML = genderMaleTotal;
-            span.appendChild(pMale);
+           
         }
 
         if (arrUsers[i].gender === "female") {
             genderFemaleTotal = genderFemaleTotal + 1;
-            const pFemale =  document.createElement("p");
-            pFemale.innerHTML = genderFemaleTotal;
-            span.appendChild(pFemale);
+           
         }
 
         if (arrUsers[i].balance > maxTotalUsersBalance) {
             maxTotalUsersBalance = arrUsers[i].balance;
-            const pUsersBalance =  document.createElement("p");
-            pUsersBalance.innerHTML = maxTotalUsersBalance;
-            span.appendChild(pUsersBalance);
+            
         }
          if (!arrUsersView) {
-            makeTable(arrUsers[i], arrUsers)
+            createUserRow(users[i], 1)
          }
 
-      return document.body.appendChild(span);
-    }
+        }
+        const pGenderMaleTotal = document.getElementById("male");
+        pGenderMaleTotal.innerText = genderMaleTotal;
+
+        const pGenderFemaleTotal = document.getElementById("female");
+        pGenderFemaleTotal.innerText = genderFemaleTotal;
+
+        const pMaxTotalUsersBalance = document.getElementById("UsersBalance");
+        pMaxTotalUsersBalance.innerText = maxTotalUsersBalance;
+
 }
