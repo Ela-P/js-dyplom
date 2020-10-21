7bfd6a9f55c1153c9d8dddb847c834f4fa791a77
Elena P <elaina.prokudova@gmail.com>
2020-10-21 14:56:41 +0300

    Found user

--- src/router.js
+++ src/router.js
@@ -4,20 +4,34 @@ import { initForm } from "./form/form";
 import "./././assets/styles/styleRoot.scss";
 
 
-const template = `<nav class= "rootNav">
-<a class= "navView" href="#/form"> Form </a>
-<a class= "navView" href="#/clients"> Clients </a>
-</nav> `;
+const template = ` <nav class= "rootNav">
+    <a class= "navView" href="#/form"> Form </a>
+    <a class= "navView" href="#/clients"> Clients </a>
+    </nav> `;
+
+
+const router = () => {
+    ROOT_NODE.innerHTML = template;
+    if (window.location.hash ===`#/clients`) {
+        logProtector(initClients);
+    } if (window.location.hash ===`#/form`) {
+        initForm();
+    } else {
+        ROOT_NODE.innerHTML =`<div>Page not found </div>`
+    }
+}
 
 export const initRouter = () => {
-    ROOT_NODE.innerHTML = template;
-    window.addEventListener("hashchange", (event) => {
-        if (window.location.hash ===`#/clients`) {
-            initClients();
-        } if (window.location.hash ===`#/form`) {
-            initForm();
-        } else {
-            ROOT_NODE.innerHTML =`<div>Page not found </div>`
-        }
-    });
+    window.addEventListener("hashchange", router);
+    router();
+};
+
+export const logProtector = (initFn) => {
+    const user = {};
+
+    if (user) {
+        initFn()
+    } else {
+        window.location.hash = `/form`;
+    }
 };
